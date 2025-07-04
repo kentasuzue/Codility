7e3b22e00204cbe7173f8beb528f00584c9621e7
Kenta Suzue <53836169+kentasuzue@users.noreply.github.com>
2025-07-04 14:12:35 -0700

    Add files via upload

---
+++ CyclicRotation.cpp
@@ -0,0 +1,77 @@
+/*
+Task description
+An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+
+The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+
+Write a function:
+
+vector<int> solution(vector<int> &A, int K);
+
+that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+
+For example, given
+
+    A = [3, 8, 9, 7, 6]
+    K = 3
+the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+
+    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+For another example, given
+
+    A = [0, 0, 0]
+    K = 1
+the function should return [0, 0, 0]
+
+Given
+
+    A = [1, 2, 3, 4]
+    K = 4
+the function should return [1, 2, 3, 4]
+
+Assume that:
+
+N and K are integers within the range [0..100];
+each element of array A is an integer within the range [−1,000..1,000].
+In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+vector<int> solution(vector<int> &A, int K) {
+    // Implement your solution here
+
+    vector<int> answer;
+    // needs to come before code with int shift to avoid divide by zero error
+    if (A.size() == 0) return answer;
+
+    // cout << "length: " << A.size() << endl;
+    // cout << "shift input: " << K << endl;
+    int shift = K % A.size();
+    // cout << "actual shift: " << shift << endl;
+    if (shift == 0) return A;
+
+
+    for (vector<int>::iterator it = A.end() - shift; it != A.end(); it++)
+    {
+        answer.push_back(*it);
+        // cout << *it << endl;
+    }
+
+    for (vector<int>::iterator it = A.begin(); it != A.end() - shift; it++)
+    {
+        answer.push_back(*it);
+    }
+
+    return answer;
+}
